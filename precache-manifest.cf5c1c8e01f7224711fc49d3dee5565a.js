8a8219eb5bb7eee4172fb09cf59e9faa19f9ea7b
Alex Chen <achen831008@gmail.com>
2018-10-13 16:36:34 -0400

    Updates

---
+++ precache-manifest.cf5c1c8e01f7224711fc49d3dee5565a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CFM-HACK-2018/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0cc1d410b8b031d3fcf8",
+    "url": "/CFM-HACK-2018/static/js/runtime~main.0cc1d410.js"
+  },
+  {
+    "revision": "4d8fb91998038982642a",
+    "url": "/CFM-HACK-2018/static/js/main.4d8fb919.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/CFM-HACK-2018/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4d8fb91998038982642a",
+    "url": "/CFM-HACK-2018/static/css/main.569f73a8.chunk.css"
+  },
+  {
+    "revision": "2fbf66d79b2bb1fa1633467a9caeb9b2",
+    "url": "/CFM-HACK-2018/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9ca3b962b8afb816de24d2ee0a2d833.js"
+  "/CFM-HACK-2018/precache-manifest.cf5c1c8e01f7224711fc49d3dee5565a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CFM-HACK-2018/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
