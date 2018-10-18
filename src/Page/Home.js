5f999f21133d795518942f60363e624c03b2228e
Alex Chen <achen831008@gmail.com>
2018-10-18 13:50:44 -0400

    update event info

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -8,36 +8,27 @@ class Home extends Component {
       <div>
         <Jumbotron>
           <Container fluid>
-            <h1 className="display-3">Hello, world!</h1>
-            <p className="lead">Date: TBD</p>
-            <p className="text-muted">Length: 3 days (Friday night to Monday morning)</p>
-            <p className="text-muted">Theme: Team Consulting</p>
-            <p className="text-muted">Participants: First year students</p>
+            <p className="lead">Date: 5:30pm Nov. 16 to 10:00am Nov. 19</p>
+            <p className="text-muted">Place: DC3336</p>
+            <p className="text-muted">Submission deadline: 10am on Nov. 19</p>
+            <p className="text-muted">Theme: Portfolio Analysis</p>
+            <p className="text-muted">Food: Pizza will be provided</p>
+            
+            <p className="text-muted">Objectives: In this hackathon, we will develop a frontend application using ReactJS. A stock json file will be provided. The file consists of the one year TSX60 stocks data. Based on the data, pick 5 stocks to build a equally-weighted portfolio. The team with the highest portfolio return in a week wins.</p>
+            <p className="text-muted">Rules:</p>
+            <p className="text-muted">- Each student will clone the starter repository and create a new team branch from the repository.</p>
+            <p className="text-muted">- To submit your code, you need to push your latest code to the team branch before the deadline.</p>
+            <p className="text-muted">- If you commit new changes after the deadline, you will be disqualified from the awards.</p>
+            <p className="text-muted">- Do not modify the data files or anything outside of your team folder!</p>
+            <p className="text-muted">- Make sure all numbers are calculated from the data file. Do not hardcode numbers! We will catch you!</p>
+            <p className="text-muted">- Project should at least include information regarding stock analysis, portfolio return, each group member's profile, and graphs/charts about the portfolio.</p>
+            
+            <p className="text-muted">Reminder: Start looking for your group (team of 4). Don't forget to bring your own laptop and charger</p>
+            <p className="text-muted">Tutorials: Git, ReactJS</p>
+            
             <p className="text-muted">Judges: CFM mentors, Yi, and Heather</p>
-            <p className="text-muted">Submission: Online (github)</p>
             <p className="text-muted">Awards: Best coding style, Best web design, Best portfolio</p>
-            <p className="text-muted">Reminder:</p>
-            <p className="text-muted">To bring your own laptop</p>
-            <p className="text-muted">We need extension cords</p>
-            <p className="text-muted">Mission: To help the new first year CFMers learn and develop new skill</p>
-
-            <p className="text-muted">Introduction Night</p>
-            <p className="text-muted">Time: Friday night 6:00pm ~ 9:00pm</p>
-            <p className="text-muted">Place: Room xxx</p>
-            <p className="text-muted">Food: Provide pizza for dinner?</p>
-
-            <p className="text-muted">We can have an introduction to this year’s CFM Hackathon (our first one). This will include the rules, submission deadline, awards, tutorials (Git, AngularJS), and support resources.</p>
-
-            <p className="text-muted">Rules: In this hackathon, we will develop a client frontend application using AngularJS. Each student will clone the starter repository and create a new branch from the repository. To submit your code, you need to push your latest code to the branch before the deadline. If you commit new changes after the deadline, you will be disqualified from the awards.</p>
-            <p className="text-muted">Submission deadline: 10am on Monday</p>
-            <p className="text-muted">Tutorials: I will prepare slides for it and do a demo too</p>
-            <p className="text-muted">Support: get support from all mentors</p>
-
-            <p className="text-muted">Awards Night</p>
-            <p className="text-muted">Time: TBD</p>
-            <p className="text-muted">Place: TBD</p>
-
-            <p className="text-muted">Give out awards to first year</p>
+            
           </Container>
         </Jumbotron>
       </div>
