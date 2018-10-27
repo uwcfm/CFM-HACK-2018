6f536e9631d5c696fbf226e01fbde2c90653f7ad
Alex Chen <achen831008@gmail.com>
2018-10-27 00:14:13 -0400

    EDIT: stlying

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -10,35 +10,35 @@ class Home extends Component {
         <h2 className="display-4">CFM Hackathon 2018</h2>
         <Jumbotron fluid>
           <Container fluid>
+            <h1 className="display-3">Kickoff</h1>
+            <p className="lead">Date: 6:00pm November 16, 2018</p>
+            <p>Place: DC3336</p>
+            <p>Food: Pizza will be provided<i class="em em-pizza"></i></p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid>
+          <Container fluid>
             <h2 className="display-3">Important Info</h2>
-            <p>Kickoff Date: 6:00pm Nov. 16</p>
-            <p>Submission deadline: 10am on Nov. 19</p>
-            <p>Theme: Portfolio Analytics</p>
-            <p>In this hackathon, we will develop a frontend application using ReactJS. A stock json file will be provided. The file consists of the one year TSX60 stocks data. Based on the data, pick 5 stocks to build a equally-weighted portfolio. The team with the highest portfolio return in a week wins.</p>
+            <p>Submission Deadline: 10:00am on November 19, 2018 (Open a pull request on Github!)</p>
+            <p>Theme: <i class="em em-chart_with_downwards_trend"></i> Portfolio Analytics <i class="em em-chart_with_upwards_trend"></i></p>
+            <p>Detail: In this hackathon, each group will develop a frontend application using ReactJS. A <a href="https://raw.githubusercontent.com/uwcfm/CFM-HACK-2018/master/src/Data/stocks.json">stock json file</a> will be provided (already in the repository). The file consists of the one year TSX60 stocks data. Based on the data, pick 5 stocks to build an equally-weighted portfolio.</p>
             <Alert color="info">
-              Reminder: Start looking for your group (team of 4). Don't forget to bring your own laptop and charger
+              Reminder: Start looking for your group (team of 4 or less). Don't forget to bring your own laptop and charger.
             </Alert>
           </Container>
         </Jumbotron>
         <Jumbotron fluid>
           <Container fluid>
-            <h2 className="display-3">Rules</h2>
+            <h2 className="display-3">Instructions</h2>
             <ListGroup>
-              <ListGroupItem color="info">Each student will clone the starter repository and create a new team branch from the repository</ListGroupItem>
-              <ListGroupItem color="info">To submit your code, you need to push your latest code to the team branch before the deadline</ListGroupItem>
-              <ListGroupItem color="info">If you commit new changes after the deadline, you will be disqualified from the awards</ListGroupItem>
-              <ListGroupItem color="danger"> <i class="em em-warning"></i>&nbsp;&nbsp;DO NOT modify the data files or anything outside of your team folder!</ListGroupItem>
-              <ListGroupItem color="info">Make sure all numbers are calculated from the data file. Do not hardcode numbers! We will catch you!</ListGroupItem>
-              <ListGroupItem color="info">Project should at least include information regarding stock analysis, portfolio return, each group member's profile, and graphs/charts about the portfolio</ListGroupItem>
+              <ListGroupItem color="info"><i class="em em-baby"></i>&nbsp;&nbsp;Each student will clone the starter <a href="https://github.com/uwcfm/CFM-HACK-2018">repository</a></ListGroupItem>
+              <ListGroupItem color="info"><i class="em em-boy"></i>&nbsp;&nbsp;Each group create a new team branch from the repository</ListGroupItem>
+              <ListGroupItem color="info"><i class="em em-man"></i>&nbsp;&nbsp;The submission from each group should at least include information regarding each group member's profile, stock analysis, portfolio return, and graphs/charts about the constructed portfolio</ListGroupItem>
+              <ListGroupItem color="info"><i class="em em-older_man"></i>&nbsp;&nbsp;Each group would push your latest code to the team branch before the deadline and open a pull request for submission</ListGroupItem>
+              <ListGroupItem color="warning"><i class="em em-warning"></i>&nbsp;&nbsp;DO NOT modify the data files or anything outside of your team folder!</ListGroupItem>
+              <ListGroupItem color="warning"><i class="em em-warning"></i>&nbsp;&nbsp;Make sure all numbers are calculated from the data file. Do not hardcode numbers! We will catch you!</ListGroupItem>
+              <ListGroupItem color="danger"><i class="em em-exclamation"></i>&nbsp;&nbsp;If a group commits changes after the deadline, such group will be disqualified from the awards</ListGroupItem>
             </ListGroup>
-          </Container>
-        </Jumbotron>
-        <Jumbotron fluid>
-          <Container fluid>
-            <h1 className="display-3">Kickoff</h1>
-            <p className="lead">Date: 6:00pm Nov. 16</p>
-            <p>Place: DC3336</p>
-            <p>Food: Pizza will be provided<i class="em em-pizza"></i></p>
           </Container>
         </Jumbotron>
         <Jumbotron fluid>
@@ -46,6 +46,7 @@ class Home extends Component {
             <h2 className="display-3">Awards</h2>
             <p>Categories: Best Coding Style, Best Web Design, Best Portfolio Return</p>
             <p>Judges: CFM mentors and staffs</p>
+            <p>Winners: <i class="em em-trophy"></i> ...TBD</p>
           </Container>
         </Jumbotron>
         <Jumbotron fluid>
@@ -59,6 +60,9 @@ class Home extends Component {
               <a href="https://www.atlassian.com/git/tutorials">Git Tutorials from Atlassian</a>,
               <a href="https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases">Git Aliases</a>,
               <a href="https://git-scm.com/book/en/v2/Git-Branching-Rebasing">Git Rebasing</a>
+            </p>
+            <p>
+              React:
             </p>
             <Link to="/example"><Button color="secondary">Learn More</Button></Link>
           </Container>
