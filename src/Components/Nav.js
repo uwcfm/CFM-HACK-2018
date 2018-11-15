92413d49ee3da6bdc0288256750b335b112f533a
martinchu <9853826+martinchu@users.noreply.github.com>
2018-11-14 22:45:21 -0500

    EDIT: nav component for teams

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+
 class NavBar extends React.Component {
   render () {
     return(
@@ -15,7 +16,16 @@ class NavBar extends React.Component {
             </DropdownToggle>
             <DropdownMenu right>
               <DropdownItem>
-                Team A
+                <NavLink tag={Link} to='/submission/team-one'>Team 1</NavLink>
+                <NavLink tag={Link} to='/submission/team-two'>Team 2</NavLink>
+                <NavLink tag={Link} to='/submission/team-three'>Team 3</NavLink>
+                <NavLink tag={Link} to='/submission/team-four'>Team 4</NavLink>
+                <NavLink tag={Link} to='/submission/team-five'>Team 5</NavLink>
+                <NavLink tag={Link} to='/submission/team-six'>Team 6</NavLink>
+                <NavLink tag={Link} to='/submission/team-seven'>Team 7</NavLink>
+                <NavLink tag={Link} to='/submission/team-eight'>Team 8</NavLink>
+                <NavLink tag={Link} to='/submission/team-nine'>Team 9</NavLink>
+                <NavLink tag={Link} to='/submission/team-ten'>Team 10</NavLink>
               </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
