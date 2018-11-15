e5daf2209d44277afad1d33cea8d26a82c4f44cb
martinchu <9853826+martinchu@users.noreply.github.com>
2018-11-15 12:35:16 -0500

    FIX: render route via a function now, instead of hard-coding

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import Example from './Page/Example';
 import {Team1, Team2, Team3, Team4, Team5, Team6, Team7, Team8, Team9, Team10} from './Page/Submission';
 
 class App extends Component {
+  renderTeam({match}){
+    // var arr=[1,2,3,3,4,5,6,7,8,9,10];
+    var teamComponentArr = [Team1, Team2, Team3, Team4, Team5, Team6, Team7, Team8, Team9, Team10];
+    const Team = teamComponentArr[match.params.id - 1];
+    return <div>
+        <Team />
+      </div>
+  }
   render() {
     return (
       <div className="App">
@@ -17,16 +25,7 @@ class App extends Component {
             <Switch>
               <Route exact path={`/`} component={Home} />
               <Route exact path={`/example`} component={Example} />
-              <Route exact path={`/submission/team-one`} component={Team1} />
-              <Route exact path={`/submission/team-two`} component={Team2} />
-              <Route exact path={`/submission/team-three`} component={Team3} />
-              <Route exact path={`/submission/team-four`} component={Team4} />
-              <Route exact path={`/submission/team-five`} component={Team5} />
-              <Route exact path={`/submission/team-six`} component={Team6} />
-              <Route exact path={`/submission/team-seven`} component={Team7} />
-              <Route exact path={`/submission/team-eight`} component={Team8} />
-              <Route exact path={`/submission/team-nine`} component={Team9} />
-              <Route exact path={`/submission/team-ten`} component={Team10} />
+              <Route path={`/submission/team/:id`} component={this.renderTeam}/>
 
               <Route render={function () {
                 return <p>Not Found</p>
