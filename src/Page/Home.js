961343d4b862fc6f1001e7568a283977e2c5794f
Phoebe <phoebe.shi2011@gmail.com>
2018-11-17 15:26:16 -0500

    I changed this to BEN

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
   render() {
     return (
       <div className='container'>
-        <h2 className="display-4">CFM Hackathon 2018</h2>
+        <h2 className="display-4">BEN</h2>
         <Jumbotron fluid>
           <Container fluid>
             <h1 className="display-3">Kickoff</h1>
