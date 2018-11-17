0849f9ab7d79badf7baf946c3851baee843291dd
Tony <antonio.ilersich@gmail.com>
2018-11-17 03:39:13 -0500

    created a stylesheet in new file StyleSheet.css, created TeamPage class in new file TeamPage.js, worked on navigation bar in Team2.js

--- src/Page/Submission/Team2/Team2.js
+++ src/Page/Submission/Team2/Team2.js
@@ -4,16 +4,23 @@
 // Aaron Kong
 
 import React, { Component } from 'react';
+//import TeamPage from "./TeamPage.js";
+import "./StyleSheet.css";
 // import Highcharts from 'highcharts';
 // import HighchartsReact from 'highcharts-react-official';
 // import { Jumbotron } from 'reactstrap';
 
 class Team2 extends Component { 
-  render() {
-    return (
-      <div className='container'>Team 2</div>
-    )
-  }
+	render() {
+		return (
+			<div id="main" className='flex-container'>
+				<h1>Team 2</h1>
+				<a href="#Home">Home</a>
+				<a href="#OurMethod">Our&nbsp;Method</a>
+				<a href="#OurTeam">Our&nbsp;Team</a>
+			</div>
+		)
+	}
 }
 
 export default Team2;
---
+++ src/Page/Submission/Team2/TeamPage.js
@@ -0,0 +1,21 @@
+// Anthony Ilersich
+// Evelyn Law
+// Lilian Chen
+// Aaron Kong
+
+import React, { Component } from 'react';
+// import Highcharts from 'highcharts';
+// import HighchartsReact from 'highcharts-react-official';
+// import { Jumbotron } from 'reactstrap';
+
+class TeamPage extends Component {
+	render() {
+		return (
+			<div id="teamPage">
+				<div><h1>OUR TEAM</h1></div>
+			</div>
+		)
+	}
+}
+
+export default TeamPage;
