00ec0540fc0604fcfd8d52da81c24ddbdbff66f1
Esther Jin <esther.vix@gmail.com>
2018-11-16 20:45:14 -0500

    "add meow"

--- src/Page/Submission/Team5/Team5.js
+++ src/Page/Submission/Team5/Team5.js
@@ -8,10 +8,10 @@ import React, { Component } from 'react';
 // import HighchartsReact from 'highcharts-react-official';
 // import { Jumbotron } from 'reactstrap';
 
-class Team5 extends Component { 
+class Team5 extends Component {
   render() {
     return (
-      <div className='container'>Team 5 hello worlds</div>
+      <div className='container'>Team 5 hello worlds meow hiii</div>
     )
   }
 }
