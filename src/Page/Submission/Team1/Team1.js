3ec1031ac5ac0cbd7f3e4abaae550f7efd92f643
Oliver <oliver.lingertat@gmail.com>
2018-11-18 17:26:12 -0500

    added a function that takes the raw data and formats it so that the graph can use it; creates a graph of closing stock prices

--- src/Page/Submission/Team1/Team1.js
+++ src/Page/Submission/Team1/Team1.js
@@ -4,8 +4,8 @@
 // Navya Mehta
 
 import React, { Component } from 'react';
-//import StockStats from '../../../Data/stocks';
-//import Alg from './Algorithm'
+import StockStats from '../../../Data/stocks';
+import Alg from './Algorithm'
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { Jumbotron } from 'reactstrap';
@@ -61,67 +61,33 @@ var options = {
         },
 }
 
-class Example extends Component {
+
+
+class Team1 extends Component {
+  
+ getClose(stockName) {
+  var closeArray = [];
+  StockStats[stockName].forEach(
+    (day) => {
+      var obj = [
+        Date.parse(day["Date"]),
+        day["Close"]
+      ];
+      closeArray.push(obj);
+    })
+    return closeArray;
+};
   
   render() {
     options['series'] = [{
         type: 'area',
-        data: [[
-            Date.parse("2017-11-01"),
-            0.7537
-          ],
-          [
-            Date.parse("2017-11-02"),
-            0.7537
-          ],
-          [
-            Date.parse("2017-11-03"),
-            0.7559
-          ],
-          [
-            Date.parse("2017-11-04"),
-            0.7631
-          ],
-          [
-            Date.parse("2017-11-05"),
-            0.7644
-          ],
-          [
-            Date.parse("2017-11-06"),
-            0.769
-          ]]
-    },{
-          data:[[
-             Date.parse("2017-11-01"),
-                      0.8537
-                    ],
-                    [
-                      Date.parse("2017-11-02"),
-                      0.8537
-                    ],
-                    [
-                      Date.parse("2017-11-03"),
-                      0.8559
-                    ],
-                    [
-                      Date.parse("2017-11-04"),
-                      0.8631
-                    ],
-                    [
-                      Date.parse("2017-11-05"),
-                      0.8644
-                    ],
-                    [
-                      Date.parse("2017-11-06"),
-                      0.869
-                    ]]
-          
-      }];
+        data: this.getClose("ABX.TO")
+    }];
     return (
       <div className='container'>
         <Jumbotron fluid>
           <div className='container'>
-            <h2>Example: NBA {Date.parse("2017-11-05")}</h2>
+            <h2>Example: NBA </h2>
             <HighchartsReact
               highcharts={Highcharts}
               options={options}
@@ -139,4 +105,4 @@ class Example extends Component {
     )
   }
 }
-export default Example;
+export default Team1;
