cda239575f2c06ede0df0c0231586c229a367671
Tony <antonio.ilersich@gmail.com>
2018-11-17 19:15:09 -0500

    created new files HomePage.js and MethodPage.js, completed navigation bar logic

---
+++ src/Page/Submission/Team2/HomePage.js
@@ -0,0 +1,21 @@
+// Anthony Ilersich
+// Evelyn Law
+// Lilian Chen
+// Aaron Kong
+
+import React, { Component } from 'react';
+// import Highcharts from 'highcharts';
+// import HighchartsReact from 'highcharts-react-official';
+// import { Jumbotron } from 'reactstrap';
+
+class HomePage extends Component {
+	render() {
+		return (
+			<div id="homePage">
+				<div><h1>HOME</h1></div>
+			</div>
+		)
+	}
+}
+
+export default HomePage;
