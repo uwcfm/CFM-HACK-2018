6f6e01743b6296d0b7ae6957b70a36149bcb3c5c
Evelyn Law <evelynlawyeeting@gmail.com>
2018-11-18 00:01:49 -0500

    i made things pretty

--- src/Page/Submission/Team2/HomePage.js
+++ src/Page/Submission/Team2/HomePage.js
@@ -12,7 +12,19 @@ class HomePage extends Component {
 	render() {
 		return (
 			<div id="homePage">
-				<div><h1>HOME</h1></div>
+				<div id="container">
+					<div id="background">
+					</div>
+				</div>
+				<div id="blurb">
+					<h1> blurb blurb blurb </h1>
+					<h2> HEADER </h2>
+					<p> another blurb about us continuing on here
+						ahhahahha so rn it's rlly random but we
+						get the point </p>
+				</div>
+				<div id="top5Summary">
+				</div>
 			</div>
 		)
 	}
